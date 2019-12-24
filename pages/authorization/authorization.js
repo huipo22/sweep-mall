48e554641188b73037a9984a08feb310cc594f9f
Your Namehuipo <huipo22@163.com>
2019-12-24 15:56:57 +0800

    4564546521212133

--- pages/authorization/authorization.js
+++ pages/authorization/authorization.js
@@ -67,15 +67,8 @@ Page({
   },
   cancelBtn() {
     //用户按了拒绝按钮
-    wx.showModal({
-      title: '警告',
-      content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
-      showCancel: false,
-      success(res) {
-        if (res.confirm) {
-          console.log('用户点击确定')
-        }
-      }
+    wx.switchTab({
+      url: '../category/category'
     })
   },
   bindGetUserInfo: function (e) {
